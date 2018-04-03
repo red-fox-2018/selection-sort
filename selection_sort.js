43c81c34a0a5f024f7b662e12fcd6b4e4df10dd3
cantdocpp <cihuyman95@gmail.com>
2018-04-03 20:03:09 +0700

    second commit

--- selection_sort.js
+++ selection_sort.js
@@ -1,3 +1,16 @@
+/*
+PSEUDOCODE
+
+1. looping dari i = 0 sepanjang array input dengan increment sebanyak 1x
+2. buat variabel untuk menampung index i saat  itu
+3. looping dari j = 1 + 1 sampai sepanjang array input dengan increment sebanyak 1x
+  3.a. jika array[i] lebih kecil dibanding dengan array index i saat  itu
+    3.a.i. ubah variabel index i saat itu menjadi index j
+4. lalu tukar posisi array[i] dengan array index ke i saat itu
+5. dan tukar array index i saat itu dengan variabel penampung array[i] yang kita buat terlebih dahulu
+6. return array
+*/
+
 function selectionSort(arr) {
   for (var i = 0; i < arr.length; i++) {
     var indexTerkecil = i
